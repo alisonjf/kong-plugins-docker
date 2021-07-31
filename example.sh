7a4f9813d8efc84ba23194eb34e41df9577c8da9
Alison Joveliano Ferrenha <aferrenha@ni-90481-6.uolcorp.intranet>
2021-07-31 20:59:20 -0300

    add url-replace plugin

--- example.sh
+++ example.sh
@@ -2,6 +2,6 @@
 docker build \
    --no-cache \
    --build-arg KONG_BASE="kong:latest" \
-   --build-arg PLUGINS="kong-plugin-jwt-validation,kong-plugin-user-profile-validation" \
+   --build-arg PLUGINS="kong-plugin-jwt-validation,kong-plugin-user-profile-validation,kong-plugin-url-replace" \
    --tag "kong-with-plugins" .
    
